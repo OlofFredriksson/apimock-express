fd4fdb72302cab2824e7215165f82075370a6061
Olof Fredriksson <oloflolof@gmail.com>
2025-04-07 13:18:26 +0200

    feat: matchResponseBrowser

--- src/browser.ts
+++ src/browser.ts
@@ -1,3 +1,8 @@
+import { getCookies } from "./browser/get-cookies";
+import { getRequestParamsFromUrl } from "./browser/get-request-params-from-url";
+import { matchResponse } from "./common";
+import { type Mock, type MockResponse } from "./mockfile";
+
 export {
     type Mock,
     type MockMatcher,
@@ -6,3 +11,40 @@ export {
     type MockRequest,
 } from "./mockfile";
 export { selectResponse, matchResponse } from "./common";
+
+/**
+ * Respond the given mockdata based by url, cookie, request parameters and headers-
+ *
+ * Major differences between this function and matchResponse is:
+ * This function will automagically retrieve cookies and request parameters
+ * Function will always return in a mock-response, the fallback will be a mocked 404 request if no given mock is matched
+ * @public
+ */
+export function matchResponseBrowser(options: {
+    mockdata: Mock[];
+    requestUrl: string;
+    method: "GET" | "POST" | "PUT" | "DELETE";
+    bodyParameters: Record<string, unknown>;
+    headers: Record<string, string | string[] | undefined>;
+}): MockResponse {
+    const mockResponse = matchResponse({
+        mockdata: options.mockdata,
+        requestUrl: options.requestUrl,
+        method: options.method,
+        requestParameters: getRequestParamsFromUrl(options.requestUrl),
+        bodyParameters: options.bodyParameters,
+        headers: options.headers,
+        cookies: getCookies(),
+    });
+
+    if (mockResponse) {
+        return mockResponse;
+    }
+
+    return {
+        label: "Mock 404 response",
+        status: 404,
+        delay: 0,
+        body: { response: "default 404 - @forsakringskassan/apimock-express" },
+    } satisfies MockResponse;
+}
---
+++ test/browser/matchResponseBrowser.spec.mjs
@@ -0,0 +1,61 @@
+// @vitest-environment happy-dom
+/* global document */
+
+import { describe, expect, test, vi } from "vitest";
+import { matchResponseBrowser } from "../../src/browser";
+import basicMock from "./basicMock.mjs";
+import basicMockPost from "./basicMockPost.mjs";
+
+describe("Browser", function () {
+    describe("matchResponseBrowser", function () {
+        const config = {
+            mockdata: [basicMock, basicMockPost],
+            requestUrl: "/private/foo/basic",
+            method: "GET",
+            bodyParameters: {},
+            headers: undefined,
+        };
+
+        test("Should return 404 response if no match", async () => {
+            const customConfig = {
+                ...config,
+                requestUrl: "404",
+            };
+            const response = matchResponseBrowser(customConfig);
+            expect(response).to.deep.equal({
+                body: {
+                    response:
+                        "default 404 - @forsakringskassan/apimock-express",
+                },
+                delay: 0,
+                label: "Mock 404 response",
+                status: 404,
+            });
+        });
+
+        test("Should get default GET-response", async () => {
+            const response = matchResponseBrowser(config);
+            expect(response).to.deep.equal({ body: { foo: "bar" } });
+        });
+
+        test("Should be able to find mocks based on cookies", async () => {
+            vi.spyOn(document, "cookie", "get").mockImplementation(
+                () => "foo=bar",
+            );
+            const response = matchResponseBrowser(config);
+            expect(response).to.deep.equal({ body: "cookies", status: 200 });
+        });
+
+        test("Should be able to find mocks query params", async () => {
+            const customConfig = {
+                ...config,
+                requestUrl: "/private/foo/basic?bar=foo&foo=bar",
+            };
+            const response = matchResponseBrowser(customConfig);
+            expect(response).to.deep.equal({
+                body: { message: "foobar" },
+                status: 401,
+            });
+        });
+    });
+});
