f0791e502b838679a26557ad1d7c3d6587e13d50
Olof Fredriksson <oloflolof@gmail.com>
2025-09-18 09:38:01 +0000

    fix: do not append fix for malformed export if it returns a string

--- src/node/extract-file-content.ts
+++ src/node/extract-file-content.ts
@@ -41,7 +41,7 @@ export async function extractFileContent(filepath: string): Promise<Mock> {
             /* this should never really be required but a malformed "default"
              * export in commonjs might (such as testcases in this repo does)
              * would behave like this as it is not a proper default export */
-            if ("default" in mock) {
+            if (typeof mock !== "string" && "default" in mock) {
                 mock = mock.default;
             }
 
